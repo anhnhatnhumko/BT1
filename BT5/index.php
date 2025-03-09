702a0e87ef3113a1b973f9eddaf18066ae5f232b
anhnhatnhumko <anhnhathuynhvo@gmail.com>
2025-03-10 01:58:54 +0700

    Initial commit

---
+++ BT5/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h2><center>This heading is in the center</h2>";
+    echo "<p>This is paragraph one and should be on top</p>";
+    echo "<hr>";
+    echo "<p>Hello</p>";
+    echo "<p>You delivered your assignment ontime.</p>";
+    echo "<p>Thanks</p>";
+    echo "<p>Mahnaz</p>";
+    echo "<hr>";
+    echo "<p>This is paragraph two and should be at bottom</p>";
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo '<div style="text-align: center; font-family: Arial, sans-serif;">';
+    echo '<div style="border: 1px solid #ccc; border-radius: 10px; padding: 20px; width: 300px; margin: 20px auto; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">';
+    echo '<img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1529699778-1509196477-sony-a6500-camera.jpg?crop=1xw:1xh;center,top" alt="Máy Ảnh Sony RX100 IV" style="width: 100%; height: auto; border-bottom: 1px solid #ddd; padding-bottom: 10px; margin-bottom: 10px;">';
+    echo '<h2 style="margin: 10px 0;text-align: left;">Máy Ảnh Sony RX100 IV (Chính Hãng)</h2>';
+    echo '<p style="font-size: 22px; margin: 10px 0; text-align: left; color: #d0021b; "><strong>19.090.000 ₫</strong> <span style="text-decoration: line-through; color: gray; font-size: 18px;">22.990.000 ₫</span> <span style="color: red; font-size: 18px;">-17%</span></p>';
+    echo '<p style="font-size: 12px; color: gray;">Source: Product of Sony</p>';
+    echo '</div>'; 
+    echo '</div>';
+    ?>
+</body>
+</html>
